domenic/worm-scraper

commit aea63ba05564b97580042213b8875e71bb6080bb

Fix inconsistently-bolded colons in Ward text conversations

lib/convert-worker.js
@@ -208,6 +208,7 @@ function getBodyXML(chapter, book, contentEl) {
   xml = xml.replace(/<strong>@<\/strong>/g, "@");
   xml = xml.replace(/<br \/>(\s*)<\/strong>/g, "</strong><br />$1");
   xml = xml.replace(/(\s*)<\/strong>/g, "</strong>$1");
+  xml = xml.replace(/><strong>(.*)<\/strong>:</g, "><strong>$1:</strong></");
 
   // No need for line breaks before paragraph ends
   // These often occur with the <br>s inside <b>/<strong>/<em>/<i> fixed above.

commit d69484af8e8481c64a1fd38a266ec09dabadfca8

Fix messed up chapters with text conversations
aea63ba05564b97580042213b8875e71bb6080bb introduced a bug where these chapters would contain ill-formed XML, and so at least some eBook readers would refuse to display them.

lib/convert-worker.js
@@ -207,7 +207,7 @@ function getBodyXML(chapter, book, contentEl) {
   xml = xml.replace(/<strong>@<\/strong>/g, "@");
   xml = xml.replace(/<br \/>(\s*)<\/strong>/g, "</strong><br />$1");
   xml = xml.replace(/(\s*)<\/strong>/g, "</strong>$1");
-  xml = xml.replace(/><strong>(.*)<\/strong>:</g, "><strong>$1:</strong></");
+  xml = xml.replace(/><strong>(.*)<\/strong>:</g, "><strong>$1:</strong><");
 
   // No need for line breaks before paragraph ends
   // These often occur with the <br>s inside <b>/<strong>/<em>/<i> fixed above.